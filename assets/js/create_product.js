18577eea5688f3bd6985c58ca4efd120376246de
70sam <hosammedo528@gmail.com>
2024-12-29 08:54:07 +0200

    Add files via upload

---
+++ assets/js/create_product.js
@@ -0,0 +1,33 @@
+window.onload = function checkLoginStatus() {
+	let loginSession = sessionStorage.getItem('loginSession');
+	if (!(loginSession && JSON.parse(loginSession).loggedIn && JSON.parse(loginSession).isAdmin)) {
+		window.location.href = 'admin_login.html';
+		return
+	}
+}
+
+// import database functions
+import { fetchData, writeData, updateData, deleteData } from "./firebase.js";
+
+document.forms[0].addEventListener('submit', (e) => {
+	e.preventDefault();
+	let productName = document.getElementById('create_product_name').value;
+	let productDescription = document.getElementById('create_product_description').value;
+	let productPrice = document.getElementById('create_product_price').value;
+	let productQuantity = document.getElementById('create_product_quantity').value;
+	let productCategory = document.getElementById('create_product_category').value;
+	let productImage = document.getElementById('create_product_image').value;
+	console.log(productImage);
+
+	let product = {
+		name: productName,
+		description: productDescription,
+		price: productPrice,
+		quantity: productQuantity,
+		category: productCategory,
+		image: productImage
+	}
+
+	writeData('products', product);
+	window.location.href = 'admin_product_view.html'
+});
---
+++ assets/js/update_product.js
@@ -0,0 +1,44 @@
+window.onload = function checkLoginStatus() {
+	let loginSession = sessionStorage.getItem('loginSession');
+	if (!(loginSession && JSON.parse(loginSession).loggedIn && JSON.parse(loginSession).isAdmin)) {
+		window.location.href = 'admin_login.html';
+		return
+	}
+}
+
+// import database functions
+import { fetchData, writeData, updateData, deleteData } from "./firebase.js";
+
+
+const params = new URLSearchParams(window.location.search);
+const productObject = fetchData('products', params.get('id'));
+
+document.getElementById('create_product_name').value = productObject.name;
+document.getElementById('create_product_description').value = productObject.description;
+document.getElementById('create_product_price').value = productObject.price;
+document.getElementById('create_product_quantity').value = productObject.quantity;
+document.getElementById('create_product_category').value = productObject.category;
+document.getElementById('create_product_image').value = productObject.image;
+
+document.forms[0].addEventListener('submit', (e) => {
+	e.preventDefault();
+	let productName = document.getElementById('create_product_name').value;
+	let productDescription = document.getElementById('create_product_description').value;
+	let productPrice = document.getElementById('create_product_price').value;
+	let productQuantity = document.getElementById('create_product_quantity').value;
+	let productCategory = document.getElementById('create_product_category').value;
+	let productImage = document.getElementById('create_product_image').value;
+	console.log(productImage);
+
+	let product = {
+		name: productName,
+		description: productDescription,
+		price: productPrice,
+		quantity: productQuantity,
+		category: productCategory,
+		image: productImage
+	}
+
+	updateData('products', product, params.get('id'));
+	window.location.href = 'admin_product_view.html'
+});
